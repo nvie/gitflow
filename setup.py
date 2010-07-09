7d1020397a064b31bf9094c1dcf984e778ee0fda
Vincent Driessen <vincent@datafox.nl>
2010-07-09 14:02:07 +0200

    Reshuffling files around the repo to facilitate new Python package layout.

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import os
+import codecs
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import gitflow as distmeta
+
+if os.path.exists("README.rst"):
+    long_description = codecs.open('README.rst', "r", "utf-8").read()
+else:
+    long_description = "See http://github.com/nvie/gitflow/tree/master"
+
+setup(
+    name="gitflow",
+    version=distmeta.__version__,
+    description="Git extensions to provide high-level repository operations for Vincent Driessen's branching model.",
+    author=distmeta.__author__,
+    author_email=distmeta.__contact__,
+    url=distmeta.__homepage__,
+    platforms=["any"],
+    license="BSD",
+    packages=find_packages(),
+    install_requires=install_requires,
+    zip_safe=False,
+    classifiers=[
+        # Picked from
+        #    http://pypi.python.org/pypi?:action=list_classifiers
+        #"Development Status :: 1 - Planning",
+        #"Development Status :: 2 - Pre-Alpha",
+        #"Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
+        #"Development Status :: 5 - Production/Stable",
+        #"Development Status :: 6 - Mature",
+        #"Development Status :: 7 - Inactive",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Version Control",
+    ],
+    long_description=long_description,
+)
