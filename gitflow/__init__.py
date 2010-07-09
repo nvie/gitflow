1a6c0c93a7a37f34bce7a9c986745453bac0e727
Vincent Driessen <vincent@datafox.nl>
2010-07-09 15:57:03 +0200

    Added versioning info.

---
+++ gitflow/__init__.py
@@ -0,0 +1,9 @@
+"""Git extensions to provide high-level repository operations for Vincent Driessen's branching model."""
+
+VERSION = (0, 0, 'pre-1')
+
+__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+__author__ = "Vincent Driessen"
+__contact__ = "vincent@datafox.nl"
+__homepage__ = "http://github.com/nvie/gitflow/"
+__docformat__ = "restructuredtext"
