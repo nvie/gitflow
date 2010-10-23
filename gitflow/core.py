cc990588c7b681c8866a31acab13e16b450e7347
Vincent Driessen <vincent@datafox.nl>
2010-10-23 04:28:20 +0200

    DRY up a few class methods.

--- gitflow/core.py
+++ gitflow/core.py
@@ -29,43 +29,19 @@ class GitFlow(object):
             if not value is None:
                 self.set(setting, value)
             else:
-                if force_defaults or self.get(setting, null=True) is None:
+                if force_defaults or not self.is_set(setting):
                     self.set(setting, default)
 
     def is_initialized(self):
-        return self.get('gitflow.branch.master', null=True) \
-           and self.get('gitflow.branch.develop', null=True) \
-           and self.get('gitflow.prefix.feature', null=True) \
-           and self.get('gitflow.prefix.release', null=True) \
-           and self.get('gitflow.prefix.hotfix', null=True) \
-           and self.get('gitflow.prefix.support', null=True)
+        return self.is_set('gitflow.branch.master') \
+           and self.is_set('gitflow.branch.develop') \
+           and self.is_set('gitflow.prefix.feature') \
+           and self.is_set('gitflow.prefix.release') \
+           and self.is_set('gitflow.prefix.hotfix') \
+           and self.is_set('gitflow.prefix.support')
 
-    def get(self, setting, default=None, null=False):
-        groups = setting.split('.', 2)
-        if len(groups) == 2:
-            subsection = None
-            section, option = groups
-        elif len(groups) == 3:
-            section, subsection, option = groups
-        else:
-            raise ValueError('Invalid setting name: %s' % setting)
 
-        try:
-            if subsection:
-                lookup_key = '%s "%s"' % (section, subsection)
-            else:
-                lookup_key = section
-            setting = self.repo.config_reader().get(lookup_key, option)
-            return setting
-        except (NoSectionError, DuplicateSectionError, NoOptionError,
-                MissingSectionHeaderError, ParsingError):
-            if null:
-                return None
-            elif not default is None:
-                return default
-            raise
-
-    def set(self, setting, value):
+    def _parse_setting(self, setting):
         groups = setting.split('.', 2)
         if len(groups) == 2:
             subsection = None
@@ -76,14 +52,34 @@ class GitFlow(object):
             raise ValueError('Invalid setting name: %s' % setting)
 
         if subsection:
-            lookup_key = '%s "%s"' % (section, subsection)
+            section = '%s "%s"' % (section, subsection)
         else:
-            lookup_key = section
+            section = section
 
+        return (section, option)
+
+    def get(self, setting, default=None, null=False):
+        section, option = self._parse_setting(setting)
+        try:
+            setting = self.repo.config_reader().get(section, option)
+            return setting
+        except (NoSectionError, DuplicateSectionError, NoOptionError,
+                MissingSectionHeaderError, ParsingError):
+            if null:
+                return None
+            elif not default is None:
+                return default
+            raise
+
+    def set(self, setting, value):
+        section, option = self._parse_setting(setting)
         writer = self.repo.config_writer()
-        if not lookup_key in writer.sections():
-            writer.add_section(lookup_key)
-        writer.set(lookup_key, option, value)
+        if not section in writer.sections():
+            writer.add_section(section)
+        writer.set(section, option, value)
+
+    def is_set(self, setting):
+        return not self.get(setting, null=True) is None
 
 
     def _safe_get(self, setting_name):
