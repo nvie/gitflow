ee9a35eed420ce811f7e0190e31e21853b30aec3
Motonori Iwata <iwata.motonori@dena.jp>
2011-09-21 10:42:10 +0900

    update Formula

--- git-flow.rb
+++ git-flow.rb
@@ -18,8 +18,8 @@ class GitFlowCompletion < Formula
 end
 
 class GitFlow < Formula
-  url 'https://github.com/iwata/gitflow.git', :tag => '0.5.8'
-  version '0.5.8'
+  url 'https://github.com/iwata/gitflow.git', :tag => '0.5.9'
+  version '0.5.9'
   head 'https://github.com/iwata/gitflow.git', :branch => 'develop'
 
   homepage 'https://github.com/iwata/gitflow'
