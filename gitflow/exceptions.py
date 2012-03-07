eb33ba545f76c98d98fbd8754d8d53b025b2d739
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-03-07 23:55:20 +0100

    Add exception `MergeError` to be raised if merge fails.

--- gitflow/exceptions.py
+++ gitflow/exceptions.py
@@ -16,6 +16,7 @@ class Usage(GitflowError):
 
 class OperationsError(GitflowError): pass
 class PrefixNotUniqueError(OperationsError): pass
+class MergeError(OperationsError): pass
 
 class StatusError(GitflowError): pass
 class WorkdirIsDirtyError(StatusError): pass
