e290610db4e6df4fb15988ac74ea5d8638d22219
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-03-07 13:16:25 +0100

    Add dummy gnupg-keying and config for testing tag-signing.
    Includes a script for regenerating the keyring.

---
+++ tests/fixtures/make-gpg-key.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+basedir=$( readlink -fn $(dirname "$0" ))
+
+gpgdir="$basedir"/gnupg
+#export GNUPGHOME="$basedir"/gnupg
+export GPG_AGENT_INFO=
+
+mkdir -p "$gpgdir"
+cd "$gpgdir"
+
+LC_ALL=C gpg --gen-key --batch <<"EOF"
+Key-Type: DSA
+%no-ask-passphrase
+%no-protection
+%transient-key
+Key-Length: 1024
+Key-Usage: sign
+Subkey-Type: ELG-E
+Subkey-Length: 1024
+Name-Real: Dummy Key for Gitflow testing
+Expire-Date: 0
+%pubring gitflow-test-dummy.pub
+%secring gitflow-test-dummy.sec
+%commit
+%echo done
+EOF
+
+export GNUPGHOME="$basedir"/gnupg
+LC_ALL=C gpg --import gitflow-test-dummy.sec gitflow-test-dummy.pub
