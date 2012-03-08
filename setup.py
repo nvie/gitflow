088f0d7428314c989eb86aa7626b7272625b9ca2
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-03-08 18:20:05 +0100

    Advance development status to "4 - Beta".

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ setup(
         # Picked from
         #    http://pypi.python.org/pypi?:action=list_classifiers
         #"Development Status :: 1 - Planning",
-        "Development Status :: 2 - Pre-Alpha",
+        #"Development Status :: 2 - Pre-Alpha",
         #"Development Status :: 3 - Alpha",
-        #"Development Status :: 4 - Beta",
+        "Development Status :: 4 - Beta",
         #"Development Status :: 5 - Production/Stable",
         #"Development Status :: 6 - Mature",
         #"Development Status :: 7 - Inactive",
