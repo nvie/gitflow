17cad61ac470b9fa8f4815aaa5e3bacbfc7a71f2
Peter van der Does <peter@avirtualhome.com>
2012-06-01 16:25:36 -0400

    No need to copy gitflow-shFlags separately.

--- contrib/msysgit-install.cmd
+++ contrib/msysgit-install.cmd
@@ -38,7 +38,6 @@ if errorlevel 4 if not errorlevel 5 goto :AccessDenied
 if errorlevel 1 set ERR=1
 xcopy "%~dp0\..\git-flow*"           "%GIT_HOME%\bin"                 /Y /R /F || set ERR=1
 xcopy "%~dp0\..\gitflow-*"           "%GIT_HOME%\bin"                 /Y /R /F || set ERR=1
-xcopy "%~dp0\..\shFlags\src\shflags" "%GIT_HOME%\bin\gitflow-shFlags" /Y /R /F || set ERR=1
 
 if %ERR%==1 choice /T 30 /C Y /D Y /M "Some unexpected errors happened. Sorry, you'll have to fix them by yourself."
 
